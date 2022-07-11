eb6cafc9376c674c9abcb3ce18ae5f4fde957b72
Salomon Chambi <salomonchambi@gmail.com>
2022-07-11 00:08:14 -0700

    Fix: parse the format specifier

--- percent_parser.c
+++ percent_parser.c
@@ -9,37 +9,37 @@
  **/
 int percent_parser(const char *format, va_list ap, int *i)
 {
-        int len, j;
+	int len, j;
 
-        t_format formats[] = {
-                {'c', is_char}, {'s', is_string},
-                /*{'d', is_integer}, {'i', is_integer},*/
-                {'\0', NULL}
-        };
+	t_format formats[] = {
+		{'c', is_char}, {'s', is_string},
+		/*{'d', is_integer}, {'i', is_integer},*/
+		{'\0', NULL}
+	};
 
-        *i = *i + 1;
+	*i = *i + 1;
 
-        if (format[*i] == '\0')
-                return (-1);
+	if (format[*i] == '\0')
+		return (-1);
 
-        if (format[*i] == '%')
-        {
-                putchar('%');
-                return (1);
-        }
+	if (format[*i] == '%')
+	{
+		putchar('%');
+		return (1);
+	}
 
-        for (len = j = 0; formats[j].specifier; j++)
-        {
-                if (format[*i] == formats[j].specifier)
-                {
-                        len = formats[j].f(ap);
-                        return (len);
-                }
+	for (len = j = 0; formats[j].specifier; j++)
+	{
+		if (format[*i] == formats[j].specifier)
+		{
+			len = formats[j].f(ap);
+			return (len);
+		}
 
-        }
+	}
 
-        putchar('%');
-        putchar(format[*i]);
+	putchar('%');
+	putchar(format[*i]);
 
-        return (2);
+	return (2);
 }
